e7cf14c410ab799353d313cbe99d5518c8bfaa79
Marc Lamberti <marclamberti@Marcs-MacBook-Pro.local>
2021-12-09 18:36:09 +0400

    user_processing

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 from datetime import datetime
 
@@ -14,7 +15,7 @@ with DAG('user_processing',
         task_id='creating_table',
         postgres_conn_id='postgres',
         sql='''
-            CREATE TABLE users (
+            CREATE TABLE IF NOT EXISTS users (
                 firstname TEXT NOT NULL,
                 lastname TEXT NOT NULL,
                 country TEXT NOT NULL,
@@ -23,5 +24,14 @@ with DAG('user_processing',
                 email TEXT NOT NULL PRIMARY KEY
             );
         '''
+    )
+    
+    is_available_api = HttpSensor(
+        task_id='is_available_api',
+        http_conn_id='user_api',
+        endpoint='api/',
+        poke_interval=60,
+        timeout=10,
+        soft_fail=False,
     )
 
