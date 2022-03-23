caa73288c3c456d1fcca80eedaa4abbefe60819b
Marc Lamberti <marclamberti@Marcs-MacBook-Pro.local>
2022-03-23 23:56:52 +0400

    push

---
+++ script_to_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install Docker
+apt-get update
+
+apt-get -y install docker.io
+
+snap install docker
+
+sudo usermod -aG docker $USER
+
+# Install VSCode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Run Airflow
+cd ambient-it
+docker-compose up -d
